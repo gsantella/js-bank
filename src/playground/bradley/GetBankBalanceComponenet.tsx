b07baf29fa67a7ae7444afb425b2e11a556b846b
btbare <btbare2002@gmail.com>
2026-01-21 16:58:46 -0500

    add component to display total balance held by the bank

---
+++ src/playground/bradley/GetBankBalanceComponenet.tsx
@@ -0,0 +1,29 @@
+import {useState} from "react"
+
+export default function GetBankBalance() {
+
+    const urlBase = "https://695f03af7f037703a8128fbf.mockapi.io/api/v1/Account"
+
+    let [count, setCount] = useState(0)
+
+    var GetCount = async function(){
+        let req = await fetch(urlBase)
+        let res = await req.json()
+        let counter = 0
+        for(let x = 0; x < res.length; x++){
+            counter += Number(res[x].balance)
+        }
+        setCount(counter)
+    }
+
+    return(
+        <>
+            <button
+            type="button"
+            onClick={GetCount}
+            >Show bank balance</button>
+
+            {count? (<p><br/>Total Cash in Bank: ${count}</p>):null}
+        </>
+    )
+}
